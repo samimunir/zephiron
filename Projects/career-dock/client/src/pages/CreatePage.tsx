7455bcf6f2d274ce051de4d1e411f996163b169c
Sami Munir <samidmunir@outlook.com>
2025-05-30 20:38:28 -0400

    implementing complete UI integration with server to create new record for resp. user

--- Projects/career-dock/client/src/pages/CreatePage.tsx
+++ Projects/career-dock/client/src/pages/CreatePage.tsx
@@ -1,7 +1,111 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 const CreatePage = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    title: "",
+    category: "",
+    company: "",
+    type: "",
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (
+      !formData.title ||
+      !formData.category ||
+      !formData.company ||
+      !formData.type
+    ) {
+      alert("Please provide all required fields.");
+      return;
+    }
+
+    const newFormData = {
+      userId: user?.id,
+      title: formData.title,
+      category: formData.category,
+      company: formData.company,
+      type: formData.type,
+    };
+
+    try {
+      const res = await fetch("http://localhost:3000/api/records/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newFormData),
+      });
+
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(data.message || "Failed to create record.");
+      }
+
+      alert("Record created successfully.");
+      navigate("/dashboard");
+    } catch (err: any) {
+      alert(err.message);
+    }
+  };
   return (
     <main className="h-[600px] border-1 border-red-500">
-      <h1>CREATE PAGE</h1>
+      <h1 className="text-center text-4xl text-zinc-100">CREATE PAGE</h1>
+      <div className="flex items-center justify-center mt-32">
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleInputChange}
+            placeholder="title"
+            className="block border-1 border-zinc-100 text-zinc-100 placeholder:text-zinc-100"
+          />
+          <input
+            type="text"
+            name="category"
+            value={formData.category}
+            onChange={handleInputChange}
+            placeholder="category"
+            className="block border-1 border-zinc-100 text-zinc-100 placeholder:text-zinc-100"
+          />
+          <input
+            type="text"
+            name="company"
+            value={formData.company}
+            onChange={handleInputChange}
+            placeholder="company"
+            className="block border-1 border-zinc-100 text-zinc-100 placeholder:text-zinc-100"
+          />
+          <input
+            type="text"
+            name="type"
+            value={formData.type}
+            onChange={handleInputChange}
+            placeholder="type"
+            className="block border-1 border-zinc-100 text-zinc-100 placeholder:text-zinc-100"
+          />
+          <button
+            type="submit"
+            className="px-2 py-1 border-1 border-red-500 text-zinc-100 bg-red-500"
+          >
+            Register
+          </button>
+        </form>
+      </div>
     </main>
   );
 };
--- Projects/career-dock/client/src/pages/DashboardPage.tsx
+++ Projects/career-dock/client/src/pages/DashboardPage.tsx
@@ -1,14 +1,60 @@
 import { useAuth } from "../context/AuthContext";
+import { useState, useEffect } from "react";
+
+type Record = {
+  _id: string;
+  title: string;
+  category: string;
+  company: string;
+  type: string;
+};
 
 const DashboardPage = () => {
   const { user } = useAuth();
+
+  const userId = user?.id;
+
+  const [records, setRecords] = useState<Record[]>([]);
+
+  const fetchRecords = async () => {
+    const res = await fetch("http://localhost:3000/api/records/user-records", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userId }),
+    });
+
+    const data = await res.json();
+
+    setRecords(data);
+  };
+
+  useEffect(() => {
+    fetchRecords();
+  }, []);
+
   return (
     <main className="h-[600px] border-1 border-red-500">
-      <h1>DASHBOARD PAGE</h1>
-      <h2>
-        {user?.firstName}, {user?.lastName}
+      <h1 className="text-center text-4xl text-zinc-300">DASHBOARD PAGE</h1>
+      <h2 className="text-3xl">
+        <span className="text-zinc-100 font-semibold">Welcome, </span>
+        <span className="text-sky-500 font-bold">
+          {user?.firstName}, {user?.lastName[0]}.
+        </span>
       </h2>
-      <p>{user?.email}</p>
+      <p className="text-2xl text-zinc-500">{user?.email}</p>
+      <div className="flex items-center justify-center gap-8 mt-8">
+        {records.map((record: Record) => (
+          <div
+            key={record._id}
+            className="bg-zinc-800 border-1 border-zinc-100 p-4 rounded-md w-[200px] hover:shadow-2xl transition-all"
+          >
+            <h1 className="text-sky-500 font-semibold">{record.title}</h1>
+            <h2 className="">{record.category}</h2>
+            <h3 className="text-zinc-100 font-bold">{record.company}</h3>
+            <h4 className="text-zinc-300">{record.type}</h4>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
