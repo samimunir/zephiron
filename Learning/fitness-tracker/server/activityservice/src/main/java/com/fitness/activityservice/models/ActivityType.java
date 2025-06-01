03d673a4008e1b7edd91b224d216a3081624113a
Sami Munir <samidmunir@outlook.com>
2025-06-01 13:57:43 -0400

    creating 2nd microservice > activity + establishing MongoDB conn

---
+++ Learning/fitness-tracker/server/activityservice/src/main/java/com/fitness/activityservice/models/ActivityType.java
@@ -0,0 +1,14 @@
+package com.fitness.activityservice.models;
+
+public enum ActivityType {
+    WALKING,
+    RUNNING,
+    SWIMMING,
+    CYCLING,
+    BOXING,
+    WEIGHT_TRAINING,
+    YOGA,
+    STRETCHING,
+    CARDIO,
+    OTHER
+}
